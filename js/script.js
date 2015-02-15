1638a218009bfcfb208f071879ec6daa5844b55e
Copeland <ncopelan@jomc08368.dhcp.unc.edu>
2015-02-15 13:16:45 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//script for sliders
+
+$(document).ready(function(){
+  console.log("DOM Ready");
+
+  jQuery(document).ready(function(){
+    jQuery('slippry-demo').slippry()
+  });
+  
+});
+
+jQuery('#news-demo').slippry({
+  // general elements & wrapper
+  slippryWrapper: '<div class="sy-box news-slider" />', // wrapper to wrap everything, including pager
+  elements: 'article', // elments cointaining slide content
+
+  // options
+  adaptiveHeight: false, // height of the sliders adapts to current 
+  captions: false,
+
+  // pager
+  pagerClass: 'news-pager',
+
+  // transitions
+  transition: 'horizontal', // fade, horizontal, kenburns, false
+  speed: 1200,
+  pause: 8000,
+
+  // slideshow
+  autoDirection: 'prev'
+});
